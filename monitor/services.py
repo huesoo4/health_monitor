cb36ef7dad1afa112da3692b842e958a65887496
huesoo4 <jlophue144@g.educaand.es>
2026-08-27 01:40:17 +0200

    feat: read json and create a list of dicts with service and status

---
+++ monitor/services.py
@@ -0,0 +1,17 @@
+import subprocess
+import json
+
+def processes():
+    with open ("/home/hueso/Documentos/health_monitor/services.json") as file:
+        f = json.load(file)
+        output = []
+        services = {}
+        for value in f.values():
+            for service in value:
+            
+                services[service] = subprocess.run(["systemctl", "is-active", service], capture_output=True).stdout
+
+            output.append(services)
+
+        return output
+
