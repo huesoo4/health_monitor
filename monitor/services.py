3b92fcc12fe10f3e8998bd014d0f5f167aea4780
huesoo4 <jlophue144@g.educaand.es>
2026-08-27 01:59:55 +0200

    refactor: remove inneccesary list and decode output

--- monitor/services.py
+++ monitor/services.py
@@ -4,14 +4,11 @@ import json
 def processes():
     with open ("/home/hueso/Documentos/health_monitor/services.json") as file:
         f = json.load(file)
-        output = []
         services = {}
         for value in f.values():
             for service in value:
             
-                services[service] = subprocess.run(["systemctl", "is-active", service], capture_output=True).stdout
+                services[service] = subprocess.run(["systemctl", "is-active", service], capture_output=True).stdout.decode('utf-8')
 
-            output.append(services)
-
-        return output
+        return services
 
