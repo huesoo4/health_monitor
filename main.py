d4bb4661cbbd6b4871ecf3b60de34fb4490b0d72
huesoo4 <jlophue144@g.educaand.es>
2026-08-26 21:08:08 +0200

    feat: add ram functions

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from monitor.hostname import get_hostname
 from monitor.cpu import *
+from monitor.ram import *
 
 print(f"""
 ================================
@@ -11,4 +12,9 @@ Hostname: {get_hostname()}
 CPU
 Usage: {get_cpu()} %
 Satus: {check_status(get_cpu())}
+
+MEMORY
+Usage: {usage_ram()} %
+Available: {available_ram()} GB
+Status: {check_status(usage_ram())}
 """)
