3ba16826bac8edd7e03355a8a7d7eaeb63adc74f
huesoo4 <jlophue144@g.educaand.es>
2026-08-27 01:56:10 +0200

    refactor: remove check function

--- monitor/ram.py
+++ monitor/ram.py
@@ -10,15 +10,3 @@ def available_ram():
     total = psutil.virtual_memory()
     return (round(total.available / 1024 / 1024 / 1024, 2))
 
-def check_status(ram):
-    if ram < 0:
-        return "ERROR"
-    elif ram >= 0 and ram <= 70:
-        return "OK"
-    elif ram > 70 and ram < 85:
-        return "WARNING"
-    elif ram >= 85 and ram <= 100:
-        return "DANGER"
-    else:
-        return "ERROR"
-
