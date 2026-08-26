17fe7ff110d90d52ea7ffd5188486dff7302bcfa
huesoo4 <jlophue144@g.educaand.es>
2026-08-27 01:41:01 +0200

    feat: add check services function

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from monitor.hostname import get_hostname
 from monitor.cpu import *
 from monitor.ram import *
 from monitor.disk import *
+from monitor.services import *
+
 
 print(f"""
 ================================
@@ -12,7 +14,7 @@ Hostname: {get_hostname()}
 
 CPU
 Usage: {get_cpu()} %
-Satus: {check_status(get_cpu())}
+Status: {check_status(get_cpu())}
 
 MEMORY
 Usage: {usage_ram()} %
@@ -23,4 +25,12 @@ DISK /
 Usage: {usage_disk()} %
 Available: {available_disk()} GB
 Status: {check_status(usage_disk())}
+
+SERVICES
 """)
+
+output = processes()
+
+for i in output:
+    for key, value in i.items():
+        print(f"{key}\t{value.decode('utf-8')}")
