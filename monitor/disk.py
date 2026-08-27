7c4783e58444e2b7942ca852ec72084b4ce7553c
huesoo4 <jlophue144@g.educaand.es>
2026-08-27 02:09:17 +0200

    refactor: better function of available disk function

--- monitor/disk.py
+++ monitor/disk.py
@@ -3,10 +3,9 @@ import psutil
 def usage_disk():
     disk = psutil.disk_usage('/')
 
-    return (round((disk.total - disk.free) / disk.total * 100, 2))
+    return (round((disk.total - disk.free) / disk.total * 100, 2), disk.free)
 
-def available_disk():
-    disk = psutil.disk_usage('/')
-    return (round(disk.free / 1024 / 1024 / 1024, 2))
+def available_disk(data):
+    return (round(data / 1024 / 1024 / 1024, 2))
 
 
