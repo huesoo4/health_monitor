c8e8f48e0db9a3b462f6511d14d5e520b05f670d
huesoo4 <jlophue144@g.educaand.es>
2026-08-27 02:09:35 +0200

    refactor: better function of available ram function

--- monitor/ram.py
+++ monitor/ram.py
@@ -3,10 +3,9 @@ import psutil
 def usage_ram():
     total = psutil.virtual_memory()
 
-    return (round((total.total - total.available) / total.total * 100, 2))
+    return (round((total.total - total.available) / total.total * 100, 2), total.available)
 
 
-def available_ram():
-    total = psutil.virtual_memory()
-    return (round(total.available / 1024 / 1024 / 1024, 2))
+def available_ram(data):
+    return (round(data / 1024 / 1024 / 1024, 2))
 
