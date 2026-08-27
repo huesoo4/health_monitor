556b0423bb54a2b48516d62b55e9fc9e2d2ec79a
huesoo4 <jlophue144@g.educaand.es>
2026-08-27 02:25:45 +0200

    refactor: delete \n of str with rstrip()

--- monitor/services.py
+++ monitor/services.py
@@ -8,14 +8,14 @@ def processes():
     parent_folder = Path(__file__).resolve().parent.parent
 
     file_folder = parent_folder / "services.json"
-    
+
     with open (file_folder) as file:
         f = json.load(file)
         services = {}
         for value in f.values():
             for service in value:
             
-                services[service] = subprocess.run(["systemctl", "is-active", service], capture_output=True).stdout.decode('utf-8')
+                services[service] = subprocess.run(["systemctl", "is-active", service], capture_output=True).stdout.decode('utf-8').rstrip()
 
         return services
 
